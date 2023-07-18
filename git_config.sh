5f15523b037aff4f0f91e02763aaa7d1f0a46cc1
dcuasapaz <diego.cuasapaz.heredia@outlook.com>
2023-07-18 12:05:17 -0500

    WC: File config repository

---
+++ git_config.sh
@@ -0,0 +1,18 @@
+# Creando folder proyecto
+mkdir git
+cd git/
+mkdir warncity
+cd warncity/
+# Iniciando carga a git
+git init
+vim README.md
+git add .
+git commit -m "WC: Inciando el proyecto"
+git branch -M main
+git remote add origin https://github.com/dcuasapaz/warncity.git
+git push -u origin main
+
+#Username for 'https://github.com': dcuasapaz
+#Password for 'https://dcuasapaz@github.com': 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
+
+
